93993d9b5208a289cb8a411f9c8331a0484c1da0
TitoNadar <titonadar06@gmail.com>
2018-04-01 16:37:26 +0530

    uploading to firebase done

--- app/src/main/java/com/example/tito/womensecurity/MainActivity.java
+++ app/src/main/java/com/example/tito/womensecurity/MainActivity.java
@@ -4,16 +4,25 @@ import android.Manifest;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.media.MediaRecorder;
+import android.net.Uri;
 import android.os.Environment;
+import android.support.annotation.NonNull;
 import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.Toast;
 
 import com.example.tito.womensecurity.Services.MyService;
 import com.github.piasy.rxandroidaudio.AudioRecorder;
 import com.github.piasy.rxandroidaudio.PlayConfig;
 import com.github.piasy.rxandroidaudio.RxAudioPlayer;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.OnProgressListener;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
 
 import java.io.File;
 
@@ -29,6 +38,8 @@ public class MainActivity extends AppCompatActivity {
     private static RxAudioPlayer mRxAudioPlayer;
     private static File mAudioFile;
   static long  a;
+  private StorageReference storageReference;
+
 
     public MainActivity() {
     }
@@ -37,6 +48,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        storageReference= FirebaseStorage.getInstance().getReference();
         if (ActivityCompat.checkSelfPermission(this, android.Manifest.permission.RECORD_AUDIO) != PackageManager.PERMISSION_GRANTED) {
 
             ActivityCompat.requestPermissions(MainActivity.this, new String[]{android.Manifest.permission.RECORD_AUDIO, Manifest.permission.WRITE_EXTERNAL_STORAGE},
@@ -69,7 +81,55 @@ for(int i=0;i<90000;i++)
 {}
         mAudioRecorder.stopRecord();
 //playRecording();
+uploadToFirebase(Environment.getExternalStorageDirectory().getAbsolutePath() +
+        File.separator +a+".file.m4a");
 }
+
+    private void uploadToFirebase(String filePath) {
+        if (filePath != null) {
+            //displaying a progress dialog while upload is going on
+
+            StorageReference riversRef = storageReference.child("audios/recordings.m4a");
+            riversRef.putFile(Uri.parse(filePath))
+                    .addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
+                        @Override
+                        public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+                            //if the upload is successfull
+                            //hiding the progress dialog
+                           Log.d("success","uploaded");
+
+                            //and displaying a success toast
+                            Toast.makeText(getApplicationContext(), "File Uploaded ", Toast.LENGTH_LONG).show();
+                        }
+                    })
+                    .addOnFailureListener(new OnFailureListener() {
+                        @Override
+                        public void onFailure(@NonNull Exception exception) {
+                            //if the upload is not successfull
+                            //hiding the progress dialog
+
+Log.d("fail","not uploaded");
+                            //and displaying error message
+                            Toast.makeText(getApplicationContext(), exception.getMessage(), Toast.LENGTH_LONG).show();
+                        }
+                    })
+                    .addOnProgressListener(new OnProgressListener<UploadTask.TaskSnapshot>() {
+                        @Override
+                        public void onProgress(UploadTask.TaskSnapshot taskSnapshot) {
+                            //calculating progress percentage
+                            double progress = (100.0 * taskSnapshot.getBytesTransferred()) / taskSnapshot.getTotalByteCount();
+Log.d("progress",progress+"");
+                            //displaying percentage in progress dialog
+
+                        }
+                    });
+        }
+        //if there is not any file
+        else {
+            //you can display an error toast
+            Log.d("error","no such file");
+        }
+    }
 
     public static void playRecording()
     {
