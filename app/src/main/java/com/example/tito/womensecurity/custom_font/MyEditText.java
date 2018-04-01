cbb4599896fd476bfbb97e7b872bac4678a6c33d
TitoNadar <titonadar06@gmail.com>
2018-04-01 17:51:15 +0530

    custom font added

---
+++ app/src/main/java/com/example/tito/womensecurity/custom_font/MyEditText.java
@@ -0,0 +1,36 @@
+package com.example.tito.womensecurity.custom_font;
+
+import android.content.Context;
+import android.graphics.Typeface;
+import android.util.AttributeSet;
+import android.widget.EditText;
+
+/**
+ * Created by Tito on 01/04/2018.
+ */
+
+public class MyEditText extends android.support.v7.widget.AppCompatEditText {
+
+    public MyEditText(Context context, AttributeSet attrs, int defStyle) {
+        super(context, attrs, defStyle);
+        init();
+    }
+
+    public MyEditText(Context context, AttributeSet attrs) {
+        super(context, attrs);
+        init();
+    }
+
+    public MyEditText(Context context) {
+        super(context);
+        init();
+    }
+
+    private void init() {
+        if (!isInEditMode()) {
+            Typeface tf = Typeface.createFromAsset(getContext().getAssets(), "fonts/Lato-Regular.ttf");
+            setTypeface(tf);
+        }
+    }
+
+}
